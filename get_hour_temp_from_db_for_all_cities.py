918aba3369b95f5e995501613d106aba941bcdbb
dmitriy <logart1995@gmail.com>
2021-02-17 21:07:01 +0200

    Optimization of create a new day script; use all days which have hour_temp for predictions now, not the last 30 days

--- get_hour_temp_from_db_for_all_cities.py
+++ get_hour_temp_from_db_for_all_cities.py
@@ -37,33 +37,29 @@ def save_hour_temp_from_db_to_csv(db, filename):
     # result = db.locations.delete_one({'_id': ObjectId(str)})
     all_dates = list(db.locations.find({}, {'date': 1, '_id': 0}))
     all_dates = [i['date'] for i in all_dates]
-    the_most_recent_date = all_dates[-1]
-    get_previous_day = db.locations.find_one({'date': the_most_recent_date})
-    number_of_cities_to_parse = len(get_previous_day['cities'])
-    prev_cities = get_previous_day['cities'][:number_of_cities_to_parse]
-    # create first dummy row
-    df = pd.DataFrame({"city": [0], "day": [0], "month": [0], "year": [0], "hour": [0], "temp": [0]})
     index = 0
     # filter out three days from previous year plus future day which can or can't contain hour_temp for all the cities
-    selected_dates = [i for i in all_dates if i > '09/10/2020'][3:]
+    # selected_dates = [i for i in all_dates if i > '09/10/2020'][3:]
+    selected_dates = [i for i in all_dates]
     selected_dates = selected_dates[::-1]
-    selected_dates = selected_dates[:30]
     print(selected_dates)
-    for selected_date in selected_dates:
-        item = db.locations.find_one({'date': selected_date})
+    all_info = db.locations.find({'date': {'$in': selected_dates}}, {'date': 1, 'cities': 1, 'hour_temp': 1})
+    d = {}
+    for item in all_info:
         cities = item['cities']
+        selected_date = item['date']
         month, day, year = selected_date.split('/')
         for city in cities:
+            if not item.get('hour_temp') or not item['hour_temp'].get(city):
+                continue
             hour_temp = item['hour_temp'][city]
             for hour in list(hour_temp.keys()):
-                index += 1
-                temp = hour_temp[hour]
+                temp = int(hour_temp[hour])
                 hour = int(hour.replace("_hour", ""))
                 new_row = {"city": city, "day": day, "month": month, "year": year, "hour": hour, 'temp': temp}
-                new_row = pd.Series(data=new_row, name=index)
-                df = df.append(new_row, ignore_index=False)
-    # remove first dummy row
-    df = df.iloc[1:]
+                d[index] = new_row
+                index += 1
+    df = pd.DataFrame.from_dict(d, 'index')
     # df.to_csv(filename)
     return df
 
